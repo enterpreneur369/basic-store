dddd5b33f3c30bb795542de8dab28d7aac5787df
José Calderón <jgcalderon372@gmail.com>
2023-10-04 19:07:34 -0500

    servicio agregado

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,17 +1,26 @@
-import { Component, Inject } from '@angular/core';
-import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
-import { Product } from '../products';
+import { Component, Inject, OnInit } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import { Product, products } from '../products';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
   constructor(
     public dialogRef: MatDialogRef<ProductDetailsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Product,
+    private route: ActivatedRoute
   ) {}
+  ngOnInit(): void {
+    const routerParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routerParams.get('productId'));
+
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
